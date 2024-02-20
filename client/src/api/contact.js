acf55c0f8b677c8f78b58494221b9b4bd2071d91
Phạm Ngọc Đạt <103233057+datpham927@users.noreply.github.com>
2024-02-20 17:07:07 +0700

    Update contact.js

--- client/src/api/contact.js
+++ client/src/api/contact.js
@@ -3,7 +3,7 @@ import axios from 'axios';
  
 const getContact = async () => {
     try {
-        const response = await axios.get(`${process.env.REACT_APP_BASE_URL}api/contact/all`);
+        const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/api/contact/all`);
         return response.data;
     } catch (error) {
         return error;
